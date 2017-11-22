94587cc66c66fd77147fc720beed52de97a7f590
Alan Cruse <alancruse86@gmail.com>
2017-11-22 22:48:06 +0100

    Add model product tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :item,  presence: true, uniqueness: { case_sensitive: false }
+  validates :packaging, presence: true
+  validates :location,  presence: true
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = Product.new(item:"Peanuts", packaging: "Partial", location: "Example Address")
+  end
+
+  test "should be vaild" do
+    assert @product.valid?
+  end
+
+  test "item should be present" do
+   @product.item = ""
+   assert_not @product.valid?
+  end
+
+  test "packaging should be present" do
+   @product.packaging = ""
+   assert_not @product.valid?
+  end
+
+  test "location should be present" do
+   @product.location = ""
+   assert_not @product.valid?
+  end
+
+  test "items should be unique" do
+    duplicate_product = @product.dup
+    duplicate_product.item = @product.item.upcase
+    @product.save
+    assert_not duplicate_product.valid?
+  end
 end
