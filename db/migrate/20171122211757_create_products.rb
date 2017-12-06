e7f41e999a7b308942646827d35bc6b95564688c
Alan Cruse <alancruse86@gmail.com>
2017-12-06 23:38:20 +0100

    Update migration file

--- db/migrate/20171122211757_create_products.rb
+++ db/migrate/20171122211757_create_products.rb
@@ -2,11 +2,11 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :item
-      t.string :packaging
-      t.text :location
+      t.string :picture
       t.text :description
-      t.text :picture
+      t.string :packaging
       t.integer :rating
+      t.text :location
       t.boolean :plastic_free
       t.timestamps
     end
