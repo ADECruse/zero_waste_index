e721b227a68664f612018593e1749a94c5706271
Alan Cruse <alancruse86@gmail.com>
2017-12-14 17:26:53 +0100

    Update JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,29 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+})
+
+function myNavFunc(){
+    // If it's an iPhone..
+    if( (navigator.platform.indexOf("iPhone") != -1)
+        || (navigator.platform.indexOf("iPod") != -1)
+        || (navigator.platform.indexOf("iPad") != -1))
+         window.open("maps://maps.google.com/maps?daddr=51.467978,-2.592091&amp;ll=");
+    else
+         window.open("http://maps.google.com/maps?daddr=51.467978,-2.592091&amp;ll=");
+}
+
 function initMap() {
         var bristol = {lat: 51.467978, lng: -2.592091};
+        var thai = {lat:51.469542, lng:-2.593643};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
           center: bristol
         });
         var marker = new google.maps.Marker({
-          position: bristol,
+          position: thai,
           map: map
         });
       }
--- db/migrate/20171122211757_create_products.rb
+++ db/migrate/20171122211757_create_products.rb
@@ -8,6 +8,8 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.integer :rating
       t.text :location
       t.boolean :plastic_free
+      t.string :latitude
+      t.string :longitude
       t.timestamps
     end
   end
