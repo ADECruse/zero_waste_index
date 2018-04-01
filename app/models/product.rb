49bfc9b40220ae9eb1e7812241f20f6af34eee59
Alan Cruse <alancruse86@gmail.com>
2018-04-01 19:16:43 +0200

    Update product controllers for image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :location_products
   has_many :locations, :through => :location_products
-
+  mount_uploader :picture, PictureUploader
   validates :item,  presence: true, uniqueness: { case_sensitive: false }
   validates :packaging, presence: true
 
