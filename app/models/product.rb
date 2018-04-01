cc82f82d3277416eb900d3f595d6c411e65e564f
Alan Cruse <alancruse86@gmail.com>
2018-04-01 19:34:16 +0200

    Add custom image upload size validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,20 @@ class Product < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :item,  presence: true, uniqueness: { case_sensitive: false }
   validates :packaging, presence: true
+  validate  :picture_size
 
 
   def self.search(search)
-  where("item ILIKE ?", "%#{search}%")
+    where("item ILIKE ?", "%#{search}%")
   end
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 
 end
